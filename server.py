c7927a4b39db6e85d54206fd524283f308088981
JayM647 <jemarmrrs@gmail.com>
2024-04-01 21:38:21 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+
+@app.route("/works.html")
+def works():
+    return render_template('works.html')
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
